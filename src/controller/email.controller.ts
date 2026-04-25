b9f824ce32dc7bf6e3d6b89a1a14a8dec8db9b1c
kartikeymishra312 <kartikmishraharsh@gmail.com>
2026-04-25 20:40:33 +0530

    controller

---
+++ src/controller/email.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { emailQueue } from "../queue/email.queue.js";
+
+export const sendEmail = async (req: Request, res: Response) => {
+  const { email } = req.body as { email: string };
+
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
+  await emailQueue.add(
+    "sendEmailJob",
+    { email },
+    {
+      attempts: 3,
+      backoff: {
+        type: "exponential",
+        delay: 1000,
+      },
+    }
+  );
+
+  res.json({
+    message: "Email job added to queue",
+  });
+};
